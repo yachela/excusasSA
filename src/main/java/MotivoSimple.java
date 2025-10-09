95cf5203b3ab3386691d81d3e41988ca6c884c05
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-09 11:38:11 -0300

    Implementar motivo concreto

--- src/main/java/MotivoSimple.java
+++ src/main/java/MotivoSimple.java
@@ -1,2 +1,6 @@
-public class MotivoSimple {
+public class MotivoSimple  implements IMotivo {
+    @Override
+    public String getJustificacion() {
+        return "Motivo trivial como quedarse dormido.";
+    }
 }
