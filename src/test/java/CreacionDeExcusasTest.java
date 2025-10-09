46a4569d20e54c2c4fe5dcfa0864059bf62b6518
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-09 11:23:39 -0300

    Crear test para que falle porque ninguna de las clases existe tdv

--- src/test/java/CreacionDeExcusasTest.java
+++ src/test/java/CreacionDeExcusasTest.java
@@ -3,4 +3,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CreacionDeExcusasTest {
 
+    @Test
+    void creadorDeExcusasTrivialesDebeCrearUnaExcusaTrivialConMotivoSimple(){
+        CreadorExcusa creador = new CreadorExcusasTriviales();
+
+        Excusa excusaCreada = creador.CrearExcusa();
+
+        AssertNotNull(excusaCreada);
+        assertInstanceOf(ExcusaTrivial.class, excusaCreada);
+        assertInstanceOf(MotivoSimple.class, excusaCreada.getMotivo());
+    }
+
 }
