304525a65cbd9ec8c936ef92e7d01ef7e8d3b2ae
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-10 12:26:11 -0300

    Agregar test de estrategiaNormal

---
+++ src/test/java/EstrategiaNormalTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EstrategiaNormalTest {
+
+    @Mock
+    private Encargado mockEncargado;
+
+    @Mock
+    private IManejadorExcusa mockSiguiente;
+
+    @Mock
+    private Excusa mockExcusa;
+
+    @Test
+    void manejarDebeLlamarAProcesarDelEncargado() {
+
+        when(mockEncargado.procesar(mockExcusa)).thenReturn(true);
+        EstrategiaNormal estrategia = new EstrategiaNormal();
+
+        estrategia.manejar(mockEncargado, mockExcusa);
+
+        verify(mockEncargado).procesar(mockExcusa);
+        verify(mockEncargado, never()).getSiguiente();
+    }
+}
