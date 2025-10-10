f2708ee5686baf3ea5874d4dcc630e2dd3a37688
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-10 11:54:19 -0300

    Agregar metodo en estategiaVaga al procesar excusa

--- src/main/java/EstrategiaVaga.java
+++ src/main/java/EstrategiaVaga.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
-public class EstrategiaVaga {
+public class EstrategiaVaga implements IEstrategiaManejo {
+    @Override
+    public void manejar(Encargado encargado, Excusa excusa){
+        if (encargado.getSiguiente() != null)
+            encargado.getSiguiente().manejarExcusa(excusa);
+    }
 }
