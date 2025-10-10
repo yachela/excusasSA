7e94b20c1f4bfdd3a3a597f964e7c3dc3de0f3e7
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-10 12:33:49 -0300

    Implementar IEstrategiaManejo y comportamiento concreto de manejar excusa en estrategia Normal

--- src/main/java/EstrategiaNormal.java
+++ src/main/java/EstrategiaNormal.java
@@ -1,2 +1,9 @@
-public class EstrategiaNormal {
+public class EstrategiaNormal implements IEstrategiaManejo {
+    @Override
+    public void manejar (Encargado encargado, Excusa excusa){
+        boolean procesada = encargado.procesar(excusa);
+        if (!procesada && encargado.getSiguiente() != null) {
+            encargado.getSiguiente().manejarExcusa(excusa);
+        }
+    }
 }
