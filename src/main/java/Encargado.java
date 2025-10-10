823a934b3bd57c0e14e7ebd1b42e5803a0194189
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-10 09:55:03 -0300

    Agregar clase Encargado

--- src/main/java/Encargado.java
+++ src/main/java/Encargado.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
 
-public class Encargado {
+public class Encargado extends Empleado implements IManejadorExcusa{
+
+    protected IManejadorExcusa siguiente;
+    private IEstrategiaManejo estrategia;
+
+    public Encargado(String nombre, String email, int legado){
+        super(nombre, email, legado);
+    }
+
+    public void setEstrategia(IEstrategiaManejo estrategia){
+        this.estrategia = estrategia;
+    }
+
+    public void setSiguiente(IManejadorExcusa siguiente){
+        this.siguiente = siguiente;
+    }
+
+    @Override
+    public void manejarExcusa(Excusa excusa){
+        if(estrategia != null){
+            this.estrategia.manejar(this, excusa);
+        }
+    }
+
+    public IManejadorExcusa getSiguiente(){
+        return siguiente;
+    }
 }
