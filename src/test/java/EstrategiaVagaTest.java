297987a7a56f6effc8e97c4fb90a5ee1d94e0d1c
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-10 11:39:10 -0300

    Agregar test para caso en que estrategia debe pasar al siguiente encargado

--- src/test/java/EstrategiaVagaTest.java
+++ src/test/java/EstrategiaVagaTest.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class EstrategiaVagaTest {
+
+    @Mock
+    private Encargado mockEncargado;
+    @Mock
+    private IManejadorExcusa mockSiguiente;
+    @Mock
+    private Excusa mockExcusa;
+
+    @Test
+    void estrategiaVagaDebePasarLaExcusaAlSiguienteEncargado(){
+        when(mockEncargado.setSiguiente()).thenReturn(mockSiguiente);
+        EstrategiaVaga estrategia = new EstrategiaVaga;
+
+        estrategia.manejar(mockEncargado, mockExcusa);
+
+        verify(mockSiguiente).manejarExcusa(mockExcusa);
+        verifyNoInteractions(mockEncargado.procesar(mockExcusa));
+    }
 }
