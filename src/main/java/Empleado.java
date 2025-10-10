90d9a835d1ee5967a056271c9ee3184d6febfe6e
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-10 09:35:15 -0300

    Agregar modelado de Empleado

--- src/main/java/Empleado.java
+++ src/main/java/Empleado.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Empleado {
+    private String nombre;
+    private String email;
+    private String legado;
+
+    public Empleado(String nombre, String email, String legado) {
+        this.nombre = nombre;
+        this.email = email;
+        this.legado = legado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getLegado() {
+        return legado;
+    }
 }
