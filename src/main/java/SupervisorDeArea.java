72ba8e14c9f030bd103d5ec16b0bbfc99d81422f
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-11 12:36:49 -0300

    Agregar SupervisorDeArea

--- src/main/java/SupervisorDeArea.java
+++ src/main/java/SupervisorDeArea.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+public class SupervisorDeArea extends Encargado {
+    private final EmailSender emailSender;
+    public SupervisorDeArea(EmailSender emailSender) {
+        super("Supervisor", "supervidor@excusas.com", 3000);
+        this.emailSender = emailSender;
 
-public class SupervisorDeArea {
+    }
+
+    @Override
+    protected boolean procesar(Excusa excusa) {
+        return false;
+    }
+
+    @Override
+    public boolean procesar(ExcusaTrivial excusa) {
+        return false;
+    }
+
+    @Override
+    public boolean procesar(ExcusaModerada excusa) {
+        IMotivo motivo = excusa.getMotivo();
+        if (motivo instanceof MotivoCorteSuministro) {
+            return true;
+        } else if (motivo instanceof MotivoCuidadoFamiliar) {
+            return true;
+        }
+        return false;
+    }
+
 }
