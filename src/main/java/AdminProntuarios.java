68be0bf987b747bde3fd9c1549ca1a11faab38a6
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-11 13:28:57 -0300

    Implementar Singleton de Admin

--- src/main/java/AdminProntuarios.java
+++ src/main/java/AdminProntuarios.java
@@ -1,2 +1,21 @@
 public class AdminProntuarios {
+
+    private static AdminProntuarios instance;
+
+    private AdminProntuarios() {
+        System.out.println("Admin de Prontuarios inicializado.");
+    }
+
+    public static AdminProntuarios getInstance() {
+        if (instance == null) {
+            instance = new AdminProntuarios();
+        }
+        return instance;
+    }
+
+    public void registrarProntuario(Excusa excusa) {
+        System.out.println("REGISTRANDO PRONTUARIO para: "
+                + excusa.getEmpleado().getNombre()
+                + " por motivo: " + excusa.getMotivo().getJustificacion());
+    }
 }
