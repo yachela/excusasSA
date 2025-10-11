3f405c226a5b2e81810a8e2cdfb3cfda75b8dd47
Laura Belén Yachelini <lbyachelini@gmail.com>
2025-10-11 13:26:55 -0300

    Crear test de CEO y Admint

---
+++ src/test/java/CEOyAdminProntuariosTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CEOyAdminProntuariosTest {
+
+    @Test
+    void adminProntuariosDebeSerSingleton() {
+        AdminProntuarios instancia1 = AdminProntuarios.getInstance();
+        AdminProntuarios instancia2 = AdminProntuarios.getInstance();
+
+        assertNotNull(instancia1);
+        assertSame(instancia1, instancia2);
+    }
+
+    @Test
+    void ceoDebeUsarAdminProntuariosAlProcesarExcusaInverosimil() {
+    }
+}
